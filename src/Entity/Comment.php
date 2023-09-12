89c6b9d2d23a9aca034dffdbf314653e9d7cc1be
SergeyArdintsev <ardintsev.s.a@gmail.com>
2023-09-12 22:55:35 +0300

    Adding conference entity and comment entity

---
+++ src/Entity/Comment.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    /**
+     * @var int|null The ID of the object.
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * @var string|null The author
+     */
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    /**
+     * @var string|null The text
+     */
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $text = null;
+
+    /**
+     * @var string|null The email
+     */
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    /**
+     * @var \DateTimeImmutable|null The created at
+     */
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    /**
+     * @var Conference|null The conference
+     */
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Conference $conference = null;
+
+    /**
+     * Retrieves the ID of the object.
+     *
+     * @return int|null The ID of the object, or null if it is not set.
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Retrieves the author of the object.
+     *
+     * @return string|null The author of the object, or null if not set.
+     */
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    /**
+     * Sets the author of the object.
+     *
+     * @param string $author The name of the author.
+     * @return static The updated object.
+     */
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Retrieves the text.
+     *
+     * @return string|null The text.
+     */
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    /**
+     * Sets the text.
+     *
+     * @param string $text
+     * @return $this The updated object.
+     */
+    public function setText(string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Retrieves the email associated with this object.
+     *
+     * @return ?string The email associated with this object, or null if no email is set.
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Sets the email for the object.
+     *
+     * @param string $email The email to set.
+     * @return static The modified object.
+     */
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Retrieves the value of the createdAt property.
+     *
+     * @return \DateTimeImmutable|null The createdAt property value.
+     */
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the value of the createdAt property.
+     *
+     * @param \DateTimeImmutable $createdAt The new value for the createdAt property.
+     * @return static Return $this for method chaining.
+     */
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Retrieves the conference associated with the current object.
+     *
+     * @return Conference|null The conference associated with the current object, or null if no conference is set.
+     */
+    public function getConference(): ?Conference
+    {
+        return $this->conference;
+    }
+
+    /**
+     * Sets the conference for the current object.
+     *
+     * @param Conference|null $conference The conference to set.
+     * @return static The updated object.
+     */
+    public function setConference(?Conference $conference): static
+    {
+        $this->conference = $conference;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Conference.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConferenceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConferenceRepository::class)]
+class Conference
+{
+    /**
+     * @var int|null The ID of the object.
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * @var string|null The city
+     */
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    /**
+     * @var string|null The year
+     */
+    #[ORM\Column(length: 4)]
+    private ?string $year = null;
+
+    /**
+     * @var bool|null Whether the conference is international
+     */
+    #[ORM\Column]
+    private ?bool $isInternational = null;
+
+    /**
+     * @var Collection|ArrayCollection Collection of comments
+     */
+    #[ORM\OneToMany(mappedBy: 'conference', targetEntity: Comment::class, orphanRemoval: true)]
+    private Collection $comments;
+
+    /**
+     * Constructor for the class.
+     *
+     * Initializes the comments property with an empty ArrayCollection.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+    /**
+     * Retrieves the ID of the object.
+     *
+     * @return int|null The ID of the object, or null if it doesn't have one.
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Retrieves the city associated with this object.
+     *
+     * @return string|null The city associated with this object, or null if no city is set.
+     */
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    /**
+     * Sets the city for the object.
+     *
+     * @param string $city The city to set.
+     * @return static Returns the updated object.
+     */
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Retrieves the year.
+     *
+     * @return ?string The year.
+     */
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    /**
+     * Sets the year for the object.
+     *
+     * @param string $year The year to set.
+     * @return static The updated object.
+     */
+    public function setYear(string $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of the $isInternational property.
+     *
+     * @return bool|null The value of the $isInternational property.
+     */
+    public function isIsInternational(): ?bool
+    {
+        return $this->isInternational;
+    }
+
+    /**
+     * Sets the value of the $isInternational property.
+     *
+     * @param bool $isInternational The new value for the $isInternational property.
+     * @return static The updated object.
+     */
+    public function setIsInternational(bool $isInternational): static
+    {
+        $this->isInternational = $isInternational;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve the comments associated with this object.
+     *
+     * @return Collection<int, Comment> The collection of comments.
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    /**
+     * Adds a comment to the current conference.
+     *
+     * @param Comment $comment The comment to be added.
+     * @throws Some_Exception_Class If the comment is already added.
+     * @return static Returns the updated Conference object.
+     */
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setConference($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Removes a comment from the comment collection.
+     *
+     * @param Comment $comment The comment to be removed.
+     * @return static Returns an instance of the class.
+     */
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getConference() === $this) {
+                $comment->setConference(null);
+            }
+        }
+
+        return $this;
+    }
+}
